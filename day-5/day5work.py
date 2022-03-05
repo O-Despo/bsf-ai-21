7210c9f3ce594cf10164429799286039e810f727
= <=>
2022-03-04 23:00:24 -0500

    Co-authored-by: noellet1 <noellet1@users.noreply.github.com> Co-authored-by: 13JoJo13 <13JoJo13@users.noreply.github.com>

---
+++ day-5/day5work.py
@@ -0,0 +1,27 @@
+# Data types
+# Transistor on or off = 1 or 0
+# 001010 = 10 
+
+# 223
+# | 2         | 2         |  3        |
+#   2 * 10^2  |  2 * 10^1 |  3 * 10^0 | 
+#   200  +  20 +  3 =   223
+
+# 101
+# | 1         | 0         |  1       |
+#   1 * 2^2  |  0 * 2^1 |  1 * 2^0 | 
+#  4   + 0  +  1 =   5
+
+# when storing data we need diffrent amounts of memory
+# Boolean = bool (True or False)
+# stwing = "good soup tik took"
+
+# anything = 21.1
+# var = True
+
+# Program that says the year
+year = 1981
+print(year)
+
+sleep = "8" + " hours"
+print(sleep)
